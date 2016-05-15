57184656e0d321f9dbba3a917f71845562aa92b2
Perchouli <jp.chenyang@gmail.com>
2016-05-16 00:00:13 +0900

    whitespace basci config

---
+++ init.el
@@ -0,0 +1,19 @@
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(require 'whitespace)
+(require 'init-whitespace)
+(global-unset-key (kbd "<left>"))
+(global-unset-key (kbd "<right>"))
+(global-unset-key (kbd "<up>"))
+(global-unset-key (kbd "<down>"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(whitespace-space ((t nil))))
---
+++ lisp/init-whitespace.el
@@ -0,0 +1,45 @@
+(setq-default show-trailing-whitespace t)
+
+
+;;; Whitespace
+
+(defun sanityinc/no-trailing-whitespace ()
+  "Turn off display of trailing whitespace in this buffer."
+  (setq show-trailing-whitespace nil))
+
+;; But don't show trailing whitespace in SQLi, inf-ruby etc.
+(dolist (hook '(special-mode-hook
+		Info-mode-hook
+		eww-mode-hook
+		term-mode-hook
+		comint-mode-hook
+		compilation-mode-hook
+		twittering-mode-hook
+		minibuffer-setup-hook))
+  (add-hook hook #'sanityinc/no-trailing-whitespace))
+
+
+;; (require-package 'whitespace-cleanup-mode)
+;; (global-whitespace-cleanup-mode t)
+(global-whitespace-mode 2)
+(setq whitespace-style (quote (spaces tabs newline space-mark tab-mark newline-mark face)))
+(setq whitespace-display-mappings
+      ;; all numbers are Unicode codepoint in decimal. ⁖ (insert-char 182 1)
+      '(
+	(space-mark 32 [183] [46]) ; 32 SPACE 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
+	(newline-mark 10 [8629 10]) ; 10 LINE FEED
+	(tab-mark 9 [9655 9] [92 9]) ; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」
+	))
+;;(set-face-attribute 'whitespace-space nil :background nil :foreground nil)
+
+
+;;(autoload 'whitespace-global-mode "whitespace"
+;;  "Toggle whitespace visualization." t
+;;  (setq whitespace-style (quote (face spaces tabs space-mark tab-mark)))
+;;  )
+
+(autoload 'whitespace-toggle-options "whitespace"
+    "Toggle load `whitespace-mode' options." t)
+(global-set-key [remap just-one-space] 'cycle-spacing)
+
+(provide 'init-whitespace)
