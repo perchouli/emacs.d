f5873c275c89b662e0c275f2ec63445781067e24
Perchouli <jp.chenyang@gmail.com>
2016-05-27 11:49:21 +0800

    User require package

--- init.el
+++ init.el
@@ -32,12 +32,13 @@
 
 (tool-bar-mode -1)
 
-(require 'window-numbering)
+(require-package 'window-numbering)
 (window-numbering-mode 1)
-(require 'multiple-cursors)
+(require-package 'multiple-cursors)
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
+(require-package 'auto-complete)
 (ac-config-default)
