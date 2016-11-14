6229e63e42b43ee9615b95e0b8b72ae009fef81f
Perchouli <jp.chenyang@gmail.com>
2016-11-14 17:08:26 +0800

    Open ido-mode; hl-line-mode

--- init.el
+++ init.el
@@ -26,10 +26,6 @@
 (setq recentf-max-saved-items 100)
 (recentf-mode 1)
 
-(global-set-key (kbd "C-S-s") 'desktop-save)
-(global-set-key (kbd "C-S-o") 'desktop-change-dir)
-;;(desktop-save-mode 1)
-
 (custom-set-variables
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
@@ -37,19 +33,16 @@
    ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(custom-enabled-themes (quote (wombat)))
  '(inhibit-startup-screen t)
-
 )
-
 
 
 (if (eq system-type 'windows-nt)
   (setq default-directory "C:/cygwin64/srv/")
-(ignore-errors
-  (set-face-attribute 'default nil :font "Consolas")
-)
   (dolist (charset '(kana han symbol cjk-misc bopomofo))
     (set-fontset-font (frame-parameter nil 'font)
   charset (font-spec :family "Microsoft YaHei" )))
+  (ignore-errors
+    (set-face-attribute 'default nil :font "Consolas"))
 )
 
 (setq-default indent-tabs-mode nil)
@@ -58,19 +51,19 @@
 (setq indent-line-function 'insert-tab)(setq org-startup-indented t)
 (setq frame-title-format "%b %f %+")
 
-
-
 (ignore-errors (tool-bar-mode -1))
-
 (require-package 'window-numbering)
 (window-numbering-mode 1)
-(require-package 'multiple-cursors)
+
+(ido-mode 1)
+(global-hl-line-mode 1)
 
 (require-package 'auto-complete)
 (ac-config-default)
+
 (setq backup-directory-alist '(("." . "~/.emacs.d/.saves")))
 
-
+(require-package 'multiple-cursors)
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
@@ -81,3 +74,6 @@
 (global-unset-key (kbd "<right>"))
 (global-set-key (kbd "C-M-n")(lambda () (interactive) (next-line 5)))
 (global-set-key (kbd "C-M-p")(lambda () (interactive) (previous-line 5)))
+
+(global-set-key (kbd "C-S-s") 'desktop-save)
+(global-set-key (kbd "C-S-o") 'desktop-change-dir)
