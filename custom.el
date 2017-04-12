e31aad2f9d2c66063afb2d3686441f29834b92e0
Perchouli <jp.chenyang@gmail.com>
2017-04-13 02:37:19 +0800

    ADD: custom.el and plugins/

---
+++ custom.el
@@ -0,0 +1,8 @@
+(custom-set-variables
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(custom-enabled-themes (quote (wombat)))
+ '(inhibit-startup-screen t)
+)
